3f17dfdc40403041fc3b7a05cc65863002262a47
Moein-Moatali-2006 <MoeinMoatali@gmail.com>
2026-06-30 23:10:36 +0330

    Add Register and Error handlers

--- apps/__init__.py
+++ apps/__init__.py
@@ -3,6 +3,7 @@ from apps.users.routes import blueprint as users_blueprint
 from apps.posts.routes import blueprint as posts_blueprint
 from apps.home.routes import blueprint as home_blueprint
 from apps.extentions import db, hashing
+import apps.exceptions as app_exception 
 
 
 def register_blueprints(app):
@@ -10,9 +11,14 @@ def register_blueprints(app):
     app.register_blueprint(posts_blueprint)
     app.register_blueprint(home_blueprint)
 
+def register_error_handlers(app):
+    app.register_error_handler(404, app_exception.page_not_found)
+    app.register_error_handler(500, app_exception.server_error)
+
 
 app = Flask(__name__)
 register_blueprints(app)
+register_error_handlers(app)
 app.config.from_object("config.DevConfig")
 
 
