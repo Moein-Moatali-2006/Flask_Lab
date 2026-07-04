bbfee1f072e890bb3fb2347913838d2f7f56d19c
Moein-Moatali-2006 <MoeinMoatali@gmail.com>
2026-07-04 23:54:08 +0330

    Add relationship database tables

--- apps/posts/routes.py
+++ apps/posts/routes.py
@@ -8,6 +8,13 @@ from apps.extentions import db
 blueprint = Blueprint("posts", __name__)
 
 @blueprint.route("/post/create", methods=["GET", "POST"])
+@login_required
 def create_post():
     form = CreatePostForm()
-    return render_template()
+    if form.validate_on_submit():
+        post = Post(title=form.title.data, body=form.body.data, author=current_user)
+        db.session.add(post)
+        db.session.commit()
+        flash("Post created..!", "success")
+        return redirect(url_for("home.home"))
+    return render_template("posts/create_post.html", form=form)
